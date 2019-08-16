b4de566c6caa41d88bd0cb26dff85ebd719f7cb3
chuanli11 <cl.chuanli@gmail.com>
2019-08-16 15:52:12 -0700

    image classification works (After a lot of hassles)

--- image_classification/tensorflow/run.sh
+++ image_classification/tensorflow/run.sh
@@ -9,12 +9,18 @@ export PYTHONPATH="$(pwd):${PYTHONPATH}"
 
 MODEL_DIR="/tmp/resnet_imagenet_${RANDOM_SEED}"
 
+# python3 official/resnet/imagenet_main.py $RANDOM_SEED --data_dir /imn/imagenet/combined/  \
+#   --model_dir $MODEL_DIR --train_epochs 10000 --stop_threshold $QUALITY --batch_size 64 \
+#   --version 1 --resnet_size 50 --epochs_between_evals 4
+
+
 python3 official/resnet/imagenet_main.py $RANDOM_SEED --data_dir /imn/imagenet/combined/  \
   --model_dir $MODEL_DIR --train_epochs 10000 --stop_threshold $QUALITY --batch_size 64 \
-  --version 1 --resnet_size 50 --epochs_between_evals 4
+  --version 1 --resnet_size 50 --epochs_between_evals 4 --num_gpus 2 --dtype fp16
 
-# To run on 8xV100s, instead run:
-#python3 official/resnet/imagenet_main.py $RANDOM_SEED --data_dir /imn/imagenet/combined/ \
-#   --model_dir $MODEL_DIR --train_epochs 10000 --stop_threshold $QUALITY --batch_size 1024 \
-#   --version 1 --resnet_size 50 --dtype fp16 --num_gpus 8 \
+# To run multiple, instead run:
+# Scale the batch size by num_gpus
+# python3 official/resnet/imagenet_main.py $RANDOM_SEED --data_dir /imn/imagenet/combined/ \
+#   --model_dir $MODEL_DIR --train_epochs 2 --stop_threshold $QUALITY --batch_size 64 \
+#   --version 1 --resnet_size 50 --dtype fp16 --num_gpus 2 \
 #   --epochs_between_evals 4
